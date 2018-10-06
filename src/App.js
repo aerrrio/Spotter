094995fcd5fcd63d467156f42589894987e0ead7
Ariel Lee <hello@aerrr.io>
2018-10-05 22:42:33 -0700

    fixed typo in the bind statement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import BusinessList from './components/BusinessList';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Yelp } from './util/Yelp';
+import './styles.css';
 
 class App extends Component {
   constructor(props) {
@@ -11,13 +13,12 @@ class App extends Component {
     this.state = {
       businesses: []
     };
-    this.Yelp = this.searchYelp.bind(this);
+    this.searchYelp = this.searchYelp.bind(this);
   }
   
-  /* Seaches Yelp and supplies this.state.businesses with information */
   searchYelp(term, location, sortBy) {
     Yelp(term, location, sortBy).then(businesses => {
-      this.setState ({ 
+      this.setState({ 
         businesses: businesses
       })
     });
